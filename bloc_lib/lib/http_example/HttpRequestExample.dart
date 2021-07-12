68d5ef96e8c719850dee8ab278ca7f91bff5bc54
hmtmcse <hmtm.cse@gmail.com>
2021-07-12 12:55:51 +0600

    BLoC Example

--- bloc_lib/lib/http_example/HttpRequestExample.dart
+++ bloc_lib/lib/http_example/HttpRequestExample.dart
@@ -1,7 +1,8 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
-class HttpRequestExample{
-
+class HttpRequestExample {
   getRequest() async {
     var url = Uri.parse("http://127.0.0.1:5000/api/v1/card/list");
     final response = await http.get(url);
@@ -22,44 +23,46 @@ class HttpRequestExample{
     print('Response body: ${response.body}');
   }
 
-  postRequest(){
+  postRequest() {}
 
+  postJsonRequest() async {
+    var url = Uri.parse('http://127.0.0.1:5000/api/v1/card/create');
+    var body = {
+      'data': {
+        "description": "postJsonRequest",
+        "name": "postJsonRequest",
+        "target": "postJsonRequest",
+        "targetType": "postJsonRequest",
+        "title": "postJsonRequest",
+        "type": "TextOnly"
+      }
+    };
+    var headers = {"Content-Type": "application/json"};
+    var encoding = Encoding.getByName("utf-8");
+    final response = await http.post(
+      url,
+      body: json.encode(body),
+      headers: headers,
+      encoding: encoding,
+    );
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
   }
 
-  postJsonRequest(){
+  requestWithHeader() {}
 
-  }
+  deleteRequest() {}
 
-  requestWithHeader(){
+  putRequest() {}
 
-  }
+  putJsonRequest() {}
 
-  deleteRequest(){
+  multiPartRequest() {}
 
-  }
-
-  putRequest(){
-
-  }
-
-
-  putJsonRequest(){
-
-  }
-
-  multiPartRequest(){
-
-  }
-
-  downloadRequest(){
-
-  }
-
+  downloadRequest() {}
 }
-
 
 void main(List<String> arguments) {
   var httpExample = HttpRequestExample();
-  httpExample.getWithParamsRequest();
+  httpExample.postJsonRequest();
 }
-
