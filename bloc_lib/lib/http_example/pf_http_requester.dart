bf48ec280e590098ddd37f782ce4e1c8060e1299
hmtmcse <hmtm.cse@gmail.com>
2021-07-15 22:40:39 +0600

    HTTP

--- bloc_lib/lib/http_example/pf_http_requester.dart
+++ bloc_lib/lib/http_example/pf_http_requester.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'dart:io';
+import 'package:path/path.dart';
 import 'package:http/http.dart' as http;
 
 class PFHttpResponse {
@@ -103,6 +105,33 @@ class PFHTTPRequester {
     return await _jsonRequest(url, DELETE, data: data);
   }
 
+
+  Future<http.MultipartFile> _getMultipartFileInfo(File file, String fieldName) async {
+    return await http.MultipartFile.fromPath(fieldName, file.path);
+  }
+
+  Future<PFHttpResponse> uploadRequest(String url, Map<String, File> files, {Map<String, String>? data, method = POST}) async {
+    var uri = Uri.parse(url);
+    var request = http.MultipartRequest(method, uri);
+    if (data is Map) {
+      request.fields.addAll(Map<String, String>.from(data!));
+    }
+
+    if (files is Map){
+      files.forEach((name, file) async {
+        if (name is String && file is File && await file.exists()){
+          http.MultipartFile multipartFile = await _getMultipartFileInfo(file, name);
+          if (multipartFile is http.MultipartFile){
+            request.files.add(multipartFile);
+          }
+        }
+      });
+    }
+
+    final response = await request.send();
+    return new PFHttpResponse(statusCode: response.statusCode, body: await response.stream.bytesToString());
+  }
+
 }
 
 
@@ -147,9 +176,19 @@ Future<void> main() async {
     }
   };
 
+  var response;
+
+  // POST upload file Request
+  var file = File("C:\\Users\\hmtmc\\Desktop\\gm\\consumer\\8.order-list.jpeg");
+  var uploadFileMap = {
+    "name": file
+  };
+  response = await PFHTTPRequester.instance().uploadRequest(baseURL + "api/v1/card/upload-file", uploadFileMap);
+  printResponse(response);
+
 
   // FORM Data POST Request
-  var response = await PFHTTPRequester.instance().POSTRequest(baseURL + "api/v1/card/form-data", data: data);
+  response = await PFHTTPRequester.instance().POSTRequest(baseURL + "api/v1/card/form-data", data: data);
   printResponse(response);
 
   // JSON POST Request
@@ -204,4 +243,6 @@ Future<void> main() async {
   // GET Request with query param
   response = await PFHTTPRequester.instance().GETRequest(baseURL + "api/v1/card/list?sort-order=asc");
   printResponse(response);
+
+
 }
