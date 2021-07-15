14b6dc762d9d6e1d986f1048717f063a630afa53
hmtmcse <hmtm.cse@gmail.com>
2021-07-15 18:59:21 +0600

    HTTP

--- bloc_lib/lib/http_example/pf_http_requester.dart
+++ bloc_lib/lib/http_example/pf_http_requester.dart
@@ -11,10 +11,15 @@ class PFHttpResponse {
 
 
 class PFHTTPRequester {
+  static const APPLICATION_JSON = "application/json";
   static const GET = "GET";
+  static const POST = "POST";
+
+
   Map<String, String> _headers = {};
 
   PFHTTPRequester addHeader(String key, String value) {
+    _headers[key] = value;
     return this;
   }
 
@@ -22,6 +27,10 @@ class PFHTTPRequester {
     return _headers;
   }
 
+  PFHTTPRequester clearHeader() {
+    _headers = {};
+    return this;
+  }
 
   Future<PFHttpResponse> requestTo(String url, String method, {Object? data}) async {
     var uri = Uri.parse(url);
@@ -30,10 +39,12 @@ class PFHTTPRequester {
     request.encoding = encoding!;
 
     if (data is String) {
-      print("String");
+      request.body = data;
     } else if (data is Map) {
-      print("Map");
+      request.bodyFields = Map<String,String>.from(data);
     }
+
+    request.headers.addAll(_headers);
 
     final response = await request.send();
     return new PFHttpResponse(statusCode: response.statusCode, body: await response.stream.bytesToString());
@@ -43,6 +54,18 @@ class PFHTTPRequester {
     return await requestTo(url, GET, data: params);
   }
 
+  Future<PFHttpResponse> POSTRequest(String url, {Map<String, String>? data}) async {
+    return await requestTo(url, POST, data: data);
+  }
+
+  Future<PFHttpResponse> jsonPOSTRequest(String url, {Map<String, dynamic>? data}) async {
+    addHeader("Content-Type", APPLICATION_JSON);
+    String? jsonString;
+    if(data is Map){
+      jsonString = json.encode(data);
+    }
+    return await requestTo(url, POST, data: jsonString);
+  }
 
 }
 
@@ -56,7 +79,34 @@ Future<void> main() async {
   var baseURL = "https://flask-hmtmcse.herokuapp.com/";
   var pfHTTPRequester = PFHTTPRequester();
 
-  var response = await pfHTTPRequester.GETRequest(baseURL + "api/v1/card/list");
+  var data = {
+    "description": "postJsonRequest",
+    "name": "postJsonRequest",
+    "target": "postJsonRequest",
+    "targetType": "postJsonRequest",
+    "title": "postJsonRequest",
+    "type": "TextOnly"
+  };
+
+  var jsonCreate = {
+    'data': {
+      "description": "postJsonRequest",
+      "name": "postJsonRequest",
+      "target": "postJsonRequest",
+      "targetType": "postJsonRequest",
+      "title": "postJsonRequest",
+      "type": "TextOnly"
+    }
+  };
+
+  var response = await pfHTTPRequester.POSTRequest(baseURL + "api/v1/card/form-data", data: data);
+  printResponse(response);
+
+  response = await pfHTTPRequester.jsonPOSTRequest(baseURL + "api/v1/card/create", data: jsonCreate);
+  printResponse(response);
+
+  pfHTTPRequester.clearHeader();
+  response = await pfHTTPRequester.GETRequest(baseURL + "api/v1/card/list");
   printResponse(response);
 
 
